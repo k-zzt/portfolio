86bd38c6789f3bdceee5a5e1e1cffce789c8d986
k-zzt <78939553+k-zzt@users.noreply.github.com>
2021-03-02 08:59:56 -0500

    a

--- styles/main.js
+++ styles/main.js
@@ -1,4 +1,15 @@
 
-let firstname='Kalsang';
+let firstname = 'Kalsang';
 
 console.log(firstname);
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  
