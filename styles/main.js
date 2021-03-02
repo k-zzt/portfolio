a062ef871efafdc75b24a3a538a81245ea1e3e20
k-zzt <78939553+k-zzt@users.noreply.github.com>
2021-03-02 10:02:35 -0500

    ,

--- styles/main.js
+++ styles/main.js
@@ -1,40 +1,10 @@
 
-let firstname='Kalsang';
-
-console.log(firstname);
-
 function welcome() {​​
 
     let username = prompt("What is your name?");    
 
-    alert("Welcome to my website " + username)
+    let message = "Welcome, " + username;
+
+    document.getElementById('saywelcome').innerHTML = message; 
 
 }​​
-$(document).ready(function () {
-	var sections      = $('section');
-	var navbar        =	$('#nav');
-	var navbarHeight  = navbar.height();
-
-	var sectionOffset = [];
-
-	sections.each(function() {
-		var section = $(this);
-
-        var position = section.offset().top - navbarHeight;
-        var backgroundColor = section.css("background-color");
-
-        sectionOffset.push({'postion': position,'color': backgroundColor});
-       
-    });
-
-	$(window).on('scroll', function(){
-		var scrollPos = $(document).scrollTop();
-
-		$.each(sectionOffset, function(index, section){
-			if (scrollPos >= section.postion) {
-				navbar.css("background-color", section.color);
-			}
-		}) 
-	});
-
-});
