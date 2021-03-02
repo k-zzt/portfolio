676fad533cbd2cec454db503261a508a61f77200
k-zzt <78939553+k-zzt@users.noreply.github.com>
2021-03-02 09:10:38 -0500

    a

--- styles/main.js
+++ styles/main.js
@@ -1,6 +1,6 @@
 
 function welcome() {
-    let firstname = prompt("What is your name?")
-    alert("Welcome to my website" + firstname)
+    let firstname = prompt("What is your name?");
+    alert("Welcome to my website" + firstname);
 }
   
