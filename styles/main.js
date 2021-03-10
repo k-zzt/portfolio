95cd598ea5f5383618c4f8b9adb5f25f6367e3f7
k-zzt <78939553+k-zzt@users.noreply.github.com>
2021-03-10 18:15:47 -0500

    Update main.js

--- styles/main.js
+++ styles/main.js
@@ -1,44 +1,44 @@
 //*Project 1*/
 function f1() {
-    alert("PROJECT 1 COMING SOON")
+    alert("PROJECT 1 COMING SOON");
 }
 
 //*Project 2*/
 function f2() {
-    alert("PROJECT 2 COMING SOON")
+    alert("PROJECT 2 COMING SOON");
 }
 
 //*Project 3*/
 function f3() {
-    alert("PROJECT 3 COMING SOON")
+    alert("PROJECT 3 COMING SOON");
 }
 
 //*Project 4*/
 function f4() {
-    alert("PROJECT 4 COMING SOON")
+    alert("PROJECT 4 COMING SOON");
 }
 
 //*Project 5*/
 function f5() {
-    alert("PROJECT 5 COMING SOON")
+    alert("PROJECT 5 COMING SOON");
 }
 
 //*Project 6*/
 function f6() {
-    alert("PROJECT 6 COMING SOON")
+    alert("PROJECT 6 COMING SOON");
 }
 
 //*Project 7*/
 function f7() {
-    alert("PROJECT 7 COMING SOON")
+    alert("PROJECT 7 COMING SOON");
 }
 
 //*Project 8*/
 function f8() {
-    alert("PROJECT 8 COMING SOON")
+    alert("PROJECT 8 COMING SOON");
 }
 
 //*Project 9*/
 function f9() {
-    alert("PROJECT 9 COMING SOON")
+    alert("PROJECT 9 COMING SOON");
 }
