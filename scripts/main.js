bdae259ea60349c17edc42f09c90ce91c6e2cbc6
k-zzt <78939553+k-zzt@users.noreply.github.com>
2021-03-22 13:47:13 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -36,9 +36,14 @@ function signofProduct() {
 document.getElementById("productsign").innerHTML = output;
 }
 
-//Sort from least to greatest
+//Sort from least to greatest. source: 
 function threeNumberSort() {
-    
+    var num1 = parseInt(prompt("Enter the first number: "));
+    var num2 = parseInt(prompt("Enter the second number: "));
+    var num3 = parseInt(prompt("Enter the third number: "));
+    var numbers = [num1 , num2 , num3];
+    numbers.sort(function(a, b){return a-b});
+    document.getElementById("numbersort").innerHTML = output;
 }
 
 //Project 4
