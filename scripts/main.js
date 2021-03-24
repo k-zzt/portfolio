d44ca302c1c13fbbb9d6bb054001b3472569d2d3
k-zzt <78939553+k-zzt@users.noreply.github.com>
2021-03-23 22:34:35 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -73,7 +73,7 @@ document.getElementById("sumthrees").innerHTML = output;
 //While loop to sum the multiples of 2 under 1000. source: https://www.w3schools.com/js/js_loop_while.asp
 function whileSumThrees() {
     var i = 0
-    while (i < 1000) {
+    while (i <= 1000) {
      if (i % 3 === 0) {
         output += i;
         i++;
